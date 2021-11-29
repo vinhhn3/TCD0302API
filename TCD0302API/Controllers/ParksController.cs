2dda803d99900dc609082c787128cee69f4e5692
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-11-29 10:26:41 +0700

    Implement ParksController (CRUD)

--- TCD0302API/Controllers/ParksController.cs
+++ TCD0302API/Controllers/ParksController.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using TCD0302API.Dtos;
+using TCD0302API.Models;
 using TCD0302API.Repositories.Interfaces;
 
 namespace TCD0302API.Controllers
@@ -38,5 +38,66 @@ namespace TCD0302API.Controllers
       return Ok(parksDto);
     }
 
+    [HttpGet("{parkId:int}", Name = "GetPark")]
+    public IActionResult GetPark(int parkId)
+    {
+      var park = _parkRepo.GetPark(parkId);
+      if (park == null) return NotFound();
+
+      var parkDto = _mapper.Map<ParkDto>(park);
+      return Ok(parkDto);
+    }
+
+    [HttpPost]
+    public IActionResult CreatePark([FromBody] ParkDto parkDto)
+    {
+      if (parkDto == null) return BadRequest(ModelState);
+
+      if (_parkRepo.ParkExists(parkDto.Name))
+      {
+        ModelState.AddModelError("", "Park Name already existed ...");
+        return BadRequest(ModelState);
+      }
+
+      var park = _mapper.Map<Park>(parkDto);
+
+      if (!_parkRepo.CreatePark(park))
+      {
+        ModelState.AddModelError("", "Something went wrong ...");
+        return BadRequest(ModelState);
+      }
+
+      return StatusCode(201);
+    }
+
+    [HttpPatch("{parkId:int}", Name = "UpdatePark")]
+    public IActionResult UpdatePark(int parkId, [FromBody] ParkDto parkDto)
+    {
+      if (parkDto == null || parkId != parkDto.Id) return BadRequest();
+
+      var park = _mapper.Map<Park>(parkDto);
+
+      if (!_parkRepo.UpdatePark(park))
+      {
+        return BadRequest("Something went wrong ...");
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("{parkId:int}", Name = "DeletePark")]
+    public IActionResult DeletePark(int parkId)
+    {
+      if (!_parkRepo.ParkExists(parkId)) return NotFound();
+
+      var park = _parkRepo.GetPark(parkId);
+
+      if (!_parkRepo.DeletePark(park))
+      {
+        return BadRequest("Something went wrong ...");
+      }
+
+      return StatusCode(204);
+    }
   }
 }
