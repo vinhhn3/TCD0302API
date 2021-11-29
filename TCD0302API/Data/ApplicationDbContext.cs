079ca135b3c92fc7cc2e2a1fb81221e9711ad674
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-11-29 08:42:40 +0700

    Setup Database Connection

---
+++ TCD0302API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCD0302API.Models;
+
+namespace TCD0302API.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+
+    }
+
+    public DbSet<Park> Parks { get; set; }
+  }
+}
