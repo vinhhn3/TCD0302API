312f9d29b24459a0d4da4d1482b760c8d68f073b
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-02 13:10:26 +0700

    Init the Client

---
+++ TCD0302.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCD0302.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
