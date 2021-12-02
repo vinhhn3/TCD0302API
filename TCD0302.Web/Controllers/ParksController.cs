ba5c8961c8d25cc2ac0c010677ba9038e15a720c
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-02 13:14:46 +0700

    Init ParkViewModel and ParksController

---
+++ TCD0302.Web/Controllers/ParksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCD0302.Web.Controllers
+{
+  public class ParksController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ TCD0302.Web/Models/ParkViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCD0302.Web.Models
+{
+  public class ParkViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+    public DateTime Established { get; set; }
+  }
+}
