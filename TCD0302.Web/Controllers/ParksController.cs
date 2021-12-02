7d9e6280fb7c3aad1900684b6392365a55e7d90f
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-02 13:36:03 +0700

    Implement Controller of Web

--- TCD0302.Web/Controllers/ParksController.cs
+++ TCD0302.Web/Controllers/ParksController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TCD0302.Web.Models;
 
 namespace TCD0302.Web.Controllers
 {
   public class ParksController : Controller
   {
-    public IActionResult Index()
+    private IHttpClientFactory _clientFactory;
+    public ParksController(IHttpClientFactory clientFactory)
     {
-      return View();
+      _clientFactory = clientFactory;
+    }
+    public async Task<IActionResult> Index()
+    {
+      var client = _clientFactory.CreateClient("ParkService");
+
+      var response = await client.GetAsync(
+        "api/parks");
+
+      if (!response.IsSuccessStatusCode) return BadRequest("Something went wrong ...");
+
+      // Convert json object to model
+      var parks = JsonConvert.DeserializeObject<List<ParkViewModel>>(
+        response.Content.ReadAsStringAsync().Result);
+
+      return View(parks);
     }
   }
 }
