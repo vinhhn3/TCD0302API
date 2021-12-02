dddb0d3f4cd843253fc3de5653d436448538843b
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-02 16:57:36 +0700

    Complete the test

--- TCD0302.Test/ParkRepositoryTest.cs
+++ TCD0302.Test/ParkRepositoryTest.cs
@@ -31,6 +31,33 @@ namespace TCD0302.Test
       mockParkRepository.Setup(m => m.GetPark(It.IsAny<int>()))
         .Returns((int i) => parks.Where(x => x.Id == i).Single());
 
+      mockParkRepository.Setup(m => m.DeletePark(It.IsAny<Park>()))
+        .Returns((Park p) => parks.Remove(p));
+
+      mockParkRepository.Setup(m => m.CreatePark(It.IsAny<Park>()))
+        .Returns((Park p) =>
+        {
+          if (p.Id.Equals(default(int)))
+          {
+            p.Id = parks.Count + 1;
+            parks.Add(p);
+            return true;
+          }
+          else return false;
+        });
+
+      mockParkRepository.Setup(m => m.UpdatePark(It.IsAny<Park>()))
+        .Returns((Park p) =>
+        {
+          var park = parks[p.Id];
+          park.State = p.State;
+          park.Name = p.Name;
+          park.Established = p.Established;
+          park.CreatedAt = p.CreatedAt;
+          return true;
+        });
+
+
       MockParkRepository = mockParkRepository.Object;
     }
 
@@ -60,6 +87,65 @@ namespace TCD0302.Test
       Assert.AreEqual(1, park.Id);
       Assert.AreEqual("State 1", park.State);
       Assert.IsInstanceOf<Park>(park);
+    }
+
+    [Test]
+    public void CanRemoveAPark()
+    {
+      // Arrange
+      var itemToRemoved = MockParkRepository.GetPark(1);
+      // Act
+      var result = MockParkRepository.DeletePark(itemToRemoved);
+
+
+      // Assert
+      Assert.IsTrue(result);
+
+      var parks = MockParkRepository.GetParks();
+      Assert.AreEqual(2, parks.Count);
+    }
+
+    [Test]
+    public void CanCreateAPark()
+    {
+      // Arrange
+
+      // Assert
+      var result = MockParkRepository.CreatePark(
+        new Park
+        {
+          Name = "Park 4",
+          State = "State 4",
+          CreatedAt = DateTime.Now,
+          Established = DateTime.Now
+        });
+      // Act
+      Assert.IsTrue(result);
+
+      var parks = MockParkRepository.GetParks();
+      Assert.AreEqual(4, parks.Count);
+
+      var park = MockParkRepository.GetPark(4);
+      Assert.AreEqual("Park 4", park.Name);
+    }
+
+    [Test]
+    public void CanUpdateAPark()
+    {
+      // Arrange
+      var park = MockParkRepository.GetPark(1);
+      park.Name = "Name 1 updated ...";
+      park.State = "State 1 updated ...";
+      // Act
+      var result = MockParkRepository.UpdatePark(park);
+
+      // Assert
+      Assert.IsTrue(result);
+
+      var parkAfterUpdate = MockParkRepository.GetPark(1);
+      Assert.AreEqual("Name 1 updated ...", parkAfterUpdate.Name);
+      Assert.AreEqual("State 1 updated ...", parkAfterUpdate.State);
+
     }
   }
 }
